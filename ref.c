96f4c0abeb16b4eac14a335eef978225dab39c36
BodomMoon <BodomMoon@gmail.com>
2018-04-08 15:13:25 +0800

    Problem solve by fix calloc

--- ref.c
+++ ref.c
@@ -5,25 +5,41 @@ typedef struct memory_pool {
 	char *tail;
 }pool;
 
-pool* init (size_t size){
-	pool *ptr = (pool*)calloc( 0 , size);
-	ptr->current = (char*) ptr;
+pool *init (size_t size){
+	/*pool *ptr = (pool*)calloc( 1 , sizeof(pool));
+	ptr->current =(char*) malloc(size);
+	ptr->tail = ptr->current + size;*/
+
+
+	pool *ptr = (pool*)calloc( 1 , size);
+	ptr->current = (char*)ptr;
 	ptr->tail = ptr->current + size;
+	ptr->current += sizeof(pool);
+
 	printf("cur = %p tai = %p\n",ptr->current,ptr->tail );
+	printf("cur place = %p value = %p\n",&(*ptr).current,ptr->current );
+	printf("tail place = %p value = %p\n",&(*ptr).tail,ptr->tail );
 	return ptr;
 }
 
-char* mpalloc(pool* ptr , size_t size) {
+char *mpalloc(pool **ptr , size_t size) {
 	//printf("get %p\n", ptr->current);
-	if((ptr -> tail - ptr -> current) > size){
-		char *temp = ptr->current;
-		ptr->current += size;
-		//printf("size = %ld temp is in %p current is in %p\n",size, temp, ptr->current);
+	if(((*ptr) -> tail - (*ptr) -> current) > size){
+		char *temp = (*ptr)->current;
+		//printf("before size = %ld temp is in %p current is in %p tail is in%p\n",size, temp, (*ptr)->current,(*ptr)->tail);
+		(*ptr)->current += size;
+		//printf("after size = %ld temp is in %p current is in %p tail is in%p\n",size, temp, (*ptr)->current,(*ptr)->tail);
 		return temp;
 	}
 	else
 		return NULL;
 	
+}
+pool *mpfreeback(pool **ptr , size_t size) {
+
+	(*ptr)->current -= size;
+	memset((*ptr)->current,0,size);
+	return (*ptr);
 }
 
 void pool_free(pool *p) 
